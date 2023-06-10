ca9b452e21f246e07d8a089bbc236a8a0b43a2ff
Harshit Rawat <harshitrawat@outlook.com>
2023-06-10 21:06:30 +0530

    Added vpc_cidr variable and igw resource

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
 resource "aws_vpc" "custom_vpc" {
-  cidr_block           = "10.123.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     Name = "custom_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "custom_igw"
   }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,9 @@ variable "ACCESS_KEY" {
 variable "SECRET_KEY" {
   description = "Secret access key of the aws user"
 }
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.123.0.0/16"
+  description = "Cidr block of custom vpc"
+}
