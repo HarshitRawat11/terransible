606352753078c132f85ddfdfe3ccef4256b8728a
Harshit Rawat <harshitrawat@outlook.com>
2023-06-11 12:04:41 +0530

    Added random id, lifecycle policy and route table

--- main.tf
+++ main.tf
@@ -4,7 +4,11 @@ resource "aws_vpc" "custom_vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name = "custom_vpc"
+    Name = "custom_vpc-${random_id.random.dec}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -12,6 +16,25 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.custom_vpc.id
 
   tags = {
-    Name = "custom_igw"
+    Name = "custom_igw-${random_id.random.dec}"
   }
+}
+
+resource "aws_route_table" "custom_public_rt" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "custom_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.custom_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+
+resource "random_id" "random" {
+  byte_length = 2
 }
